a91d1ab03d0c37e203f9d7fa674f8e6885337e22
Zyel Crier <zyel.crier@gmail.com>
2020-06-26 21:34:44 +0000

    Set up Servlets

---
+++ capstone/src/main/java/com/google/sps/servlets/UserServlet.java
@@ -0,0 +1,11 @@
+package com.google.sps.servlets;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+    //TODO doGet for getting user info
+    //TODO doPost for creating and storing a user account
+}
