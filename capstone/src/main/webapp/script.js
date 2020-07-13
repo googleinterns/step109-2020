583d981043588191fbe58def94e119be5ee7162f
Zyel Crier <zyel.crier@gmail.com>
2020-07-13 19:09:17 +0000

    MVP Search with CSS changes

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -6,3 +6,46 @@ var instance = M.Tabs.init(elem, options);
 //Initialize sidenav
 const slide_menu = document.querySelectorAll(".sidenav");
 M.Sidenav.init(slide_menu, {});
+
+//Get Search Request From User and display result
+async function studySetsBySearch() {
+  const stringToSearchBy = document.getElementById("search").value;
+  const response = await fetch(
+    "/study_set?stringToSearchBy=" + stringToSearchBy
+  );
+  const searchResponse = await response.json();
+  var configuredHTMLSearchResponse = configureSearchResponseToHTML(
+    searchResponse
+  );
+  document.getElementById(
+    "results-container"
+  ).innerHTML = configuredHTMLSearchResponse;
+}
+
+function configureSearchResponseToHTML(studySets) {
+  var configuredStudySetInfoCards = "";
+  if (studySets.length == 0) {
+    return ` <div class="no-results"><h3>No Results Found...<i class="large material-icons">sentiment_dissatisfied</i></h3></div>`;
+  }
+
+  for (var studySet of studySets) {
+    configuredStudySetInfoCards += formatCardInHTML(studySet);
+  }
+  return configuredStudySetInfoCards;
+}
+
+function  formatCardInHTML(studySet) { 
+  return `<a href="/viewStudySet.html?id=${studySet.id}" class="result-card">
+      <div class="row result-card"> 
+        <div class="col card s12 m12 l12">
+          <div class="top-line">
+            <span><strong> ${studySet.title.toUpperCase()}
+            </strong>, ${studySet.subject}</span>
+          </div>
+          <p>${studySet.study_set_length} cards in set</p>
+          <p><strong>DESCRIPTION</strong>, ${studySet.description}</p>
+          <span> By  ${studySet.user_author} from ${studySet.university}</span>
+         </div>
+      </div>
+    </a>`;
+}
